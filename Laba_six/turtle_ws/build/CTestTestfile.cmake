0c3cc7ef47759edb7927bdb846b41484a4ade99e
Kibotenok <nikitaantropov@inbox.ru>
2020-04-20 10:29:46 +0900

    Do not work

---
+++ Laba_six/turtle_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aantropov/WORK/Study/PO/Labs/RoboticsPO/Laba_six/turtle_ws/src
+# Build directory: /home/aantropov/WORK/Study/PO/Labs/RoboticsPO/Laba_six/turtle_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("id_painter")
---
+++ Laba_six/turtle_ws/build/id_painter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aantropov/WORK/Study/PO/Labs/RoboticsPO/Laba_six/turtle_ws/src/id_painter
+# Build directory: /home/aantropov/WORK/Study/PO/Labs/RoboticsPO/Laba_six/turtle_ws/build/id_painter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
