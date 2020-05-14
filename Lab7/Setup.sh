5c6b23cae6c29dc1aa0ae8167e106d064df6e187
Kibotenok <nikitaantropov@inbox.ru>
2020-05-15 07:41:08 +0900

    First setup

---
+++ Lab7/Setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Лабораторная работа №4
+# Настройка окружающей среды
+
+path_choice()
+{
+	# Проверяет введенный пользователем путь до целевого каталога
+
+	if [ "${1:0:1}" = "/" ] || [ "${1:0:1}" = "./" ] ; then
+		
+		if [ -d "$1" ] ; then
+			goal_dir="$1"
+		
+		elif [ -f "$1" ] ; then
+			echo "Error: Expected directory, not file"
+			exit 1
+
+		else
+			echo "Error: No such directory"
+			exit 1
+		fi
+	else
+		goal_dir=$("pwd")
+	fi
+}
+
+######################
+# ОСНОВНАЯ ПРОГРАММА #
+######################
+
+cur_dir="$(pwd)" # Рабочая директория
+path_choice "$1" 
+cd "$goal_dir/maze_ws"
+
+# Настройка окружения и запуск ROS
+source devel/setup.bash
+roscd wall_explorer/
+chmod u+x src/id_painter_node/IdPainter.py
+roslaunch wall_explorer maze.launch
+
+cd "$cur_dir" > /dev/null
+exit 0
---
+++ Lab7/maze_ws/build/wall_explorer/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "wall_explorer"
+PROJECT_SPACE_DIR = "/home/aantropov/WORK/Study/PO/RoboticsPO/Lab7/maze_ws/devel"
+PROJECT_VERSION = "0.0.0"
